5d1c5343a4e069f9926118a240bc99cf470cfc92
twozeronine <amdusias209@gmail.com>
2021-05-08 22:12:31 +0900

    level1 / 하샤드수 다른 사람 풀이 추가

--- Programmers/level1/하샤드 수.cs
+++ Programmers/level1/하샤드 수.cs
@@ -3,4 +3,20 @@ using System.Linq;
 public class HashadNumber
 {
   public static bool solution(int x) => x % x.ToString().Select(e => e - '0').Sum() == 0 ? true : false;
+
+
+
+  //다른 사람의 풀이
+  public bool other_solution(int x)
+  {
+    int sum = 0;
+    int temp = x;
+    while (temp > 0)
+    {
+      sum += temp % 10;
+      temp = temp / 10;
+    }
+
+    return x % sum == 0;
+  }
 }
